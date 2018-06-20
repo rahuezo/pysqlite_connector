7f10336c1fb5ef9941e47ed3d3d8839cd7cf5863
Rudy Huezo <rahuezo@ucdavis.edu>
2018-06-20 07:00:44 -0700

    Finished database package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='sqlite_database', 
+    version='0.1',
+    description='A simple Database class to connect to sqlite and make queries.',
+    url='https://github.com/rahuezo/pysqlite_connector',
+    author='Rudy Huezo',
+    author_email='rahuezo@ucdavis.edu',
+    license='MIT',
+    packages=['database'],
+    zip_safe=False
+)
